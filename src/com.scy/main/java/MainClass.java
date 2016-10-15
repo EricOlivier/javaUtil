796ec2ef2b6d727e56dc4481006a5e97402809dc
Shen chuyu <ericolivier@163.com>
2016-10-16 02:14:27 +0800

    update

--- src/com.scy/main/java/MainClass.java
+++ src/com.scy/main/java/MainClass.java
@@ -1,13 +1,10 @@
 /**
  * Created by SCY on 16/10/16.
  */
-public class HelloWorld {
+public class MainClass {
 
     public static void main(String[] args) throws Exception {
         System.out.println("hello world");
-
-
-
     }
 
 
