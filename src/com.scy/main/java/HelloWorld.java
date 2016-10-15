e74a6da7894d4a2e812ba052f3ee269fd4959fbf
Shen chuyu <ericolivier@163.com>
2016-10-16 02:06:35 +0800

    Initial commit

---
+++ src/com.scy/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Created by SCY on 16/10/16.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello world");
+
+
+
+    }
+
+
+}
