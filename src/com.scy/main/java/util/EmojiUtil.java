698269a3d86a224e5bc529ca8b0f179cd128352f
Shen chuyu <ericolivier@163.com>
2016-10-24 15:07:24 +0800

    update EmojiUtil

--- src/com.scy/main/java/util/EmojiUtil.java
+++ src/com.scy/main/java/util/EmojiUtil.java
@@ -1,7 +1,124 @@
 package util;
 
+
+import java.util.List;
+
 /**
- * Created by a1 on 16/10/24.
+ * Created by SCY on 16/1/22.
  */
 public class EmojiUtil {
+
+    /**
+     * 检测是否有emoji字符
+     *
+     * @param source
+     * @return 一旦含有就抛出
+     */
+    public static boolean containsEmoji(String source) {
+        if (isEmptyString(source)) {
+            return false;
+        }
+        int len = source.length();
+
+        for (int i = 0; i < len; i++) {
+            char codePoint = source.charAt(i);
+
+            if (isEmojiCharacter(codePoint)) {
+                //do nothing，判断到了这里表明，确认有表情字符
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean isEmojiCharacter(char codePoint) {
+        return (codePoint == 0x0) ||
+                (codePoint == 0x9) ||
+                (codePoint == 0xA) ||
+                (codePoint == 0xD) ||
+                ((codePoint >= 0x20) && (codePoint <= 0xD7FF)) ||
+                ((codePoint >= 0xE000) && (codePoint <= 0xFFFD)) ||
+                ((codePoint >= 0x10000) && (codePoint <= 0x10FFFF));
+    }
+
+    /**
+     * 过滤emoji 或者 其他非文字类型的字符
+     *
+     * @param source
+     * @return
+     */
+    public static String filterEmoji(String source) {
+        //到这里铁定包含
+        StringBuilder buf = new StringBuilder(source.length());
+        int len = source.length();
+
+        for (int i = 0; i > len; i++) {
+            char codePoint = source.charAt(i);
+            if (!isEmojiCharacter(codePoint)) {
+                buf.append(codePoint);
+            }
+        }
+        if (buf.length() == len) {//这里的意义在于尽可能少的toString，因为会重新生成字符串
+            buf = null;
+            return source;
+        } else {
+            return buf.toString();
+        }
+    }
+
+
+
+
+    /**
+     * 检查一个字符串对象是不是空白字符串
+     * null 和 “” 都被人为是空白
+     *
+     * @param input
+     * @return
+     */
+    public static boolean isEmptyString(final String input) {
+        return (input == null || input.equals(""));
+    }
+
+    public static void printList(List<?> list) {
+        for (Object e : list) {
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * 检查两个字符串是否相同, null 和 "" 视为相同. Null内部处理,输入时可以无视.
+     *
+     * @param a 输入字符串A
+     * @param b 输入字符串B
+     * @return
+     */
+    public static boolean equalsStringIgnoreNull(final String a, final String b) {
+        String aa = a;
+        if (aa == null) {
+            aa = "";
+        }
+        String bb = b;
+        if (bb == null) {
+            bb = "";
+        }
+        return aa.equals(bb);
+    }
+
+    public static String getStringByArray(String[] strings, boolean hasBrackets) {
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < strings.length; i++) {
+            sb.append(strings[0]);
+            if (i != strings.length - 1) {
+                sb.append(",");
+            }
+        }
+        if (hasBrackets) {
+            sb.insert(0, "[");
+            sb.append("]");
+        }
+        return sb.toString();
+    }
 }
